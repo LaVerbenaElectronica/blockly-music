87a82013b1c678f9346ce6ae2ba3e97000c05cd7
Jesús Jara López <LaVerbenaElectronica@users.noreply.github.com>
2024-12-12 20:13:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -336,10 +336,10 @@ Blockly.JavaScript['poly_note'] = function (block) {
   synth` + num + `.set({oscillator: {type: '${waveType}'}});
   synth0.set({envelope: {attack: 1, decay: 0.15, sustain: 1, release: 1}});`;
   if (kind == 'harm') {
-    code = code + `synth` + num + `.triggerAttackRelease(['${note}', '${note}' * 2, '${note}' * 3, '${note}' * 4], `+ dur + `, now + ` + timeDur + `);`;
+    code = code + `synth` + num + `.triggerAttackRelease(['${note}', '${note}' * 2, '${note}' * 3, '${note}' * 4], `+ dur + `, now + ` + timeDur + `, 0.5);`;
   }
   else {
-    code = code + `synth` + num + `.triggerAttackRelease(['${note}', '${note}' * 2.76, '${note}' * 5.40, '${note}' * 8.93], `+ dur + `, now + ` + timeDur + `);`;
+    code = code + `synth` + num + `.triggerAttackRelease(['${note}', '${note}' * 2.76, '${note}' * 5.40, '${note}' * 8.93], `+ dur + `, now + ` + timeDur + `, 0.5);`;
   }  
   num++;
   return code;
